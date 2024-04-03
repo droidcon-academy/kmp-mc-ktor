5f341e54d1af201a983ce7e487e923fa41d675a4
Stefan Jovanovic <stefan.jovanavich@gmail.com>
2024-04-03 09:08:09 +0200

    - UnsplashDemo starter project added.

---
+++ source-code/starter-project/UnsplashDemo/composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,8 @@
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    // TODO: The main entry point of our application for both iOS and Android
+}
---
+++ source-code/starter-project/UnsplashDemo/composeApp/src/commonMain/kotlin/api/UnsplashApi.kt
@@ -0,0 +1,8 @@
+package api
+
+class UnsplashApi {
+    companion object {
+        const val UNSPLASH_URL = "https://api.unsplash.com"
+        const val ACCESS_KEY = "YOUR_ACCESS_KEY"
+    }
+}
